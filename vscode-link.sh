b43c740f1867b3617bf550ee8b194ef1618281ab
Max von Webel <max@343max.de>
2024-04-17 14:19:44 +0200

    allowing custom hostname

--- vscode-link.sh
+++ vscode-link.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 WORKSPACE="$(cd "${1:-$PWD}" && pwd -P)"
-HOSTNAME="$(/bin/hostname)"
+if [ -n "$VSCODELINK_HOST" ]; then
+    HOSTNAME="$VSCODELINK_HOST"
+else
+    HOSTNAME="$(/bin/hostname)"
+fi
 URL="vscode://vscode-remote/ssh-remote+$HOSTNAME$WORKSPACE?windowId=_blank"
 echo $URL
